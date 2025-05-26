4b3b6e69551872649f74687f75caad67754d42e5
Eamon <168888004+eamon047@users.noreply.github.com>
2025-05-26 16:02:32 +0900

    结构调整

--- Java/Assignment6/DrawGraphics.java
+++ Java/Assignment6/DrawGraphics.java
@@ -1,18 +1,41 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class DrawGraphics {
-    Bouncer movingSprite;
+    // ArrayList<Bouncer> movingSprite = new ArrayList<>();
+    // ArrayList<StraightMover> straightSprite = new ArrayList<>();
+
+    ArrayList<Mover> movers = new ArrayList<>();
 
     /** Initializes this class for drawing. */
     public DrawGraphics() {
-        Rectangle box = new Rectangle(15, 20, Color.RED);
-        movingSprite = new Bouncer(100, 170, box);
-        movingSprite.setMovementVector(3, 1);
+        Rectangle rect1 = new Rectangle(15, 20, Color.RED);
+        Bouncer b1 = new Bouncer(100, 170, rect1);
+        b1.setMovementVector(3, 1);
+
+        Oval oval1 = new Oval(30, 30, Color.BLUE);
+        Bouncer b2 = new Bouncer(50, 150, oval1);
+        b2.setMovementVector(2, 3);
+
+        Rectangle rect2 = new Rectangle(15, 20, Color.RED);
+        StraightMover b3 = new StraightMover(180, 60, rect2);
+        b3.setMovementVector(3, 1);
+
+        Oval oval2 = new Oval(30, 30, Color.BLUE);
+        StraightMover b4 = new StraightMover(200, 50, oval2);
+        b4.setMovementVector(2, 3);
+
+        movers.add(b1);
+        movers.add(b2);
+        movers.add(b3);
+        movers.add(b4);
     }
 
     /** Draw the contents of the window on surface. */
     public void draw(Graphics surface) {
-        movingSprite.draw(surface);
+        for (Mover b : movers) {
+            b.draw(surface);
+        }
     }
 }
---
+++ Java/Assignment6/Mover.java
@@ -0,0 +1,5 @@
+import java.awt.Graphics;
+
+public interface Mover {
+    void draw(Graphics surface);
+}
